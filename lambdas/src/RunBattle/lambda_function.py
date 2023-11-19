08973a175a6a551c7405424c81671f33a86b3817
MichaelKramps <mdk989@gmail.com>
2023-11-19 14:46:40 -0600

    update lambda function with new animations class syntax

--- lambdas/src/RunBattle/lambda_function.py
+++ lambdas/src/RunBattle/lambda_function.py
@@ -5,13 +5,14 @@ from Animations import Animations
 
 def lambda_handler(event, context):
     # TODO implement
-    simulator = DogfightSimulator(event["deckOne"], event["deckTwo"], Animations())
+    animations = Animations()
+    simulator = DogfightSimulator(event["deckOne"], event["deckTwo"], animations)
     simulator.simulateDogfight()
     return {
         'statusCode': 200,
         'body': {
             'startingDeckOne': simulator.startingDeckOne,
             'startingDeckTwo': simulator.startingDeckTwo,
-            'animationString': '*'.join(Animations.animationsList)
+            'animationString': '*'.join(animations.animationsList)
         }
     }
